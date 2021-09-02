57286cae301b4c05d5e9d7f9f302d0974ac53614
Sunakshi Jain <swtsunakshi19@gmail.com>
2021-09-02 10:08:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Giphy from './giphy'
+import react, {useState} from 'react'
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isDarkTheme? 'App dark':'App'}>
+      <div className="demo">
+          <div className="demoSearchBox">
+            <div className="searchBox">
+              <Giphy
+                apiKey="9Ixlv3DWC1biJRI57RanyL7RTbfzz0o7"
+                // eslint-disable-next-line no-console
+                onSelect={item => console.log(item)}
+                library="stickers"
+                searchPlaceholder="Search for Stickers"
+                masonryConfig={[
+                  { columns: 2, imageWidth: 110, gutter: 5 },
+                  { mq: '700px', columns: 3, imageWidth: 400, gutter: 5 },
+                ]}
+                gifListHeight={800}
+              />
+            </div>
+          </div>
+          </div>
+          <div>
+        <input type="checkbox" className="checkbox" onChange={()=>{setIsDarkTheme(!isDarkTheme)}} id="checkbox" />
+        <label htmlFor="checkbox" className="label">
+          <i className="fas fa-moon" />
+          <i className="fas fa-sun" />
+          <div className="ball">
+          </div>
+        </label></div>
     </div>
   );
 }
